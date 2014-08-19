c07505af144bba83784174cef715bc82298c8468
María Isabel Joaquín Hernández <maisabeljh@yahoo.com.mx>
2014-08-19 10:53:16 -0500

    Dos A
    Cambio de una letra c por t

--- js/acciones.js
+++ js/acciones.js
@@ -6,7 +6,7 @@ $(document).ready(function(e){
 			navigator.notification.beep(1);
 		}); //tap beep
 		$('#vibrar').tap(function(){
-			navigator.notificacion.vibrate(1000);
+			navigator.notification.vibrate(1000);
 		});// tap vibrar
 	},false); //deviceready
 });//ready
